69a122a481badc9c7dc879fd2b969c88df77fd8f
dekalouis <dekarebaudo@gmail.com>
2026-03-08 23:20:12 +0700

    feat: added styling and set up proper version

--- src/dsa_cli/display.py
+++ src/dsa_cli/display.py
@@ -1,7 +1,9 @@
 from loader import Question
+from rich import print
+from rich.panel import Panel
 
 def show_question(question: Question) -> None:
-    print(question.title)
+    print(Panel(question.title, title="Question"))
 
     steps = question.steps
 
@@ -9,11 +11,12 @@ def show_question(question: Question) -> None:
     #     print(step)
     #     input('Next?')
     for i, step in enumerate(steps):
-        print(f'{i+1}. {step}')
-        input('->')
-    print('Summary')
-    print(f'Time Complexity: {question.time_complexity}')
-    print(f'Space Complexity: {question.space_complexity}')
+        print(f'[bold green]{i+1}. {step}[/bold green]')
+        print("[dim]-> Press Enter for next step...[/dim]", end="")
+        input()
+    
+    summary = f"[bold cyan]Summary\nTime Complexity: {question.time_complexity}\nSpace Complexity: {question.space_complexity}[/bold cyan]"
+    print(Panel(summary))
 
 
 # show_question(Question(id='0', title='Contains Duplicates', category='Arrays & Hashing', difficulty='easy', steps=['Create an empty set/hashmap to store seen numbers', 'Loop the entire list of numbers', 'If the number exists in the hashmap, return True', 'Otherwise, add it to the hashmap'], time_complexity='O(n)', space_complexity='O(n)'),)
