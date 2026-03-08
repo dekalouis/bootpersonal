9bf28023f969d3c87d362f933ad9b5ab720cfd30
dekalouis <dekarebaudo@gmail.com>
2026-03-08 20:00:50 +0700

    feat: prepared main for others

--- src/dsa_cli/main.py
+++ src/dsa_cli/main.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+from loader import load_question, Question
+from display import show_question
+import json
+
+BASE_DIR = Path(__file__).parent    
+QUESTION_PATH = BASE_DIR / '../../questions/questions.json'
+
 def main():
     print("Hello from personal-project!")
 
